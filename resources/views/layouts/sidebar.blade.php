52e068ed9a2702b8a5047defcf16ad4d40a54abd
jenlut <jenlut.jl@gmail.com>
2022-09-12 19:12:24 +0700

    templating

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,25 @@
+<div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+      <div class="sidebar-brand">
+        <a href="index.html">Toko Produk </a>
+      </div>
+      <div class="sidebar-brand sidebar-brand-sm">
+        <a href="index.html">Tp</a>
+      </div>
+      <ul class="sidebar-menu">
+          <li class="menu-header">Dashboard</li>
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Produk</span></a>
+            <ul class="dropdown-menu">
+              <li><a class="nav-link" href="index-0.html">Product</a></li>
+              <li><a class="nav-link" href="index.html">Categories</a></li>
+              <li><a class="nav-link" href="index.html">Variant</a></li>
+              <li><a class="nav-link" href="index.html">Product Images</a></li>
+            </ul>
+          </li>
+  
+          <li><a class="nav-link" href="credits.html"><i class="fas fa-pencil-ruler"></i> <span>Api</span></a></li>
+        </ul>
+
+    </aside>
+  </div>
